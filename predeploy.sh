e923cbe66f266d0134d39866606a1cb3f123795e
Alxandr <alxandr@alxandr.me>
2016-01-12 13:40:45 +0100

    debugging

--- predeploy.sh
+++ predeploy.sh
@@ -2,6 +2,7 @@
 
 npm run build
 cp package.json package.json.orig
+echo "Travis tag: $TRAVIS_TAG"
 if [[ ! -z "$TRAVIS_TAG" ]]; then
   json -If package.json -e "this.version += '-build.$TRAVIS_BUILD_NUMBER'"
 fi
