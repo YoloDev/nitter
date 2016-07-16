317f5d1e069707ed1034a2b56da33e3175604fd2
Alxandr <alxandr@alxandr.me>
2016-07-16 23:52:13 +0200

    Document map module

--- src/map.js
+++ src/map.js
@@ -1,3 +1,7 @@
+/**
+ *
+ */
+
 import { makeNitterFn, subtype } from './nitter';
 
 const mapType = subtype('Nitter:Map', (n, fn) => {
@@ -15,6 +19,25 @@ const mapType = subtype('Nitter:Map', (n, fn) => {
   };
 });
 
+/**
+ * {@link map} calls a provided callback function *for each element* in an iterable, in order, and
+ * constructs a new iterable from the results. This does not mutate the original iterable, but
+ * rather creates a new one.
+ *
+ * @function map
+ * @summary The {@link map} function creates a new iterable with the result of calling a provided
+ *          function on every element in the provided iterable.
+ * @param {mapCallback} callback Function that produces an element of the new iterable.
+ * @returns {iterable} A new iterable where each element is the result of calling the provided
+ *                     function on the old iterable.
+ *
+ * @playground
+ * // Testing map
+ * const { map, toArray } = require('nitter');
+ * const arr = [1, 2, 3];
+ * const mapped = map(i => i * 2)(arr);
+ * toArray()(mapped);
+ */
 export const map = makeNitterFn(function map(n, fn) { // eslint-disable-line prefer-arrow-callback
   if (typeof fn !== 'function') {
     throw new Error(`Expected fn to be a function, got ${fn}.`);
@@ -22,3 +45,11 @@ export const map = makeNitterFn(function map(n, fn) { // eslint-disable-line pre
 
   return mapType(n, fn);
 });
+
+/**
+ * Function that produce an element of the new iterable, taking 1 argument:
+ *
+ * @callback mapCallback
+ * @param {*} currentValue The current element being processed in the array.
+ * @inner
+ */
