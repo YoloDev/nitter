b288bd858a35bbe1d7ca55905675a0a607a14b3a
Alxandr <alxandr@alxandr.me>
2016-07-20 15:50:00 +0200

    Document count module

--- src/count.js
+++ src/count.js
@@ -1,5 +1,20 @@
+/**
+ *
+ */
+
 import { makeNitterFn } from './nitter';
 
+/**
+ * Counts the length of a iterable. If the iterable is really long, this will halt your application.
+ *
+ * @function count
+ * @returns {number} The number of items in the iterable.
+ * @playground
+ * // Testing count
+ * const { count } = require('nitter');
+ * const arr = [1, 2, 3];
+ * count()(arr);
+ */
 export const count = makeNitterFn(function count(n) { //eslint-disable-line prefer-arrow-callback
   const arr = n.arr();
   if (arr !== null) {
